28ba707e69707869f479d3356a42cdd2a8317cfb
phish77 <benjamin.goetzinger@web.de>
2024-11-02 11:03:09 +0100

    Update install_portainer.sh

--- runfirst/install_portainer.sh
+++ runfirst/install_portainer.sh
@@ -13,7 +13,7 @@ docker run hello-world
 docker volume create portainer_data
 
 # Portainer-Container starten
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v portainer_data:/data \
-  portainer/portainer-ce:latest
+# docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always \
+#   -v /var/run/docker.sock:/var/run/docker.sock \
+#   -v portainer_data:/data \
+#   portainer/portainer-ce:latest
